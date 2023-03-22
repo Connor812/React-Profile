64abcde3c245fdf6e8aaed936387d9a5e53a26fb
Connor812 <connorsavoy812@gmail.com>
2023-03-21 22:17:31 -0400

    added the about me information as well as a profile image

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import profileImg from '../../assets/images/profile-picture.jpg'
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
-    </div>
-  );
+    return (
+        <div className='center'>
+            <div className='about-me-box'>
+                <div className='about-me'>
+                    About Me
+                </div>
+                <img className='profile-img' src={profileImg} alt='profile' />
+                <div className='description'>
+                    <div>
+                        Hello everyone! My name is Connor Savoy and I am from Langton Ontario! I'm currently enrolled at the University of Toronto in a full-stack coding bootcamp. I enrolled in this course because of my love for technology and computers. Coding is an amazing way to expand on those horizons and build something amazing from at first glace, looks likea bunch of mumbo-gumbo.
+                    </div>
+                    <div>
+                        I love the problem solving aspect of coding. It always keeps me on my toes and looking for new inovative ways to write code and solve my problems.
+                    </div>
+                    <div>
+                        As of right now my favourite code to write is back-end server side scripts. I like coding the databases and writing out complex querys to sort and manuver through a database.
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
