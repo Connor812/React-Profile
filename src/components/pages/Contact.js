c2cb1a6d4f65a88795426e69b120ebeedc067546
Connor812 <connorsavoy812@gmail.com>
2023-03-23 23:00:55 -0400

    added some comments to easy distingish the sections

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import '../../styles/contact.css';
+
 
 export default function Contact() {
     const [name, setName] = useState('');
@@ -30,26 +32,40 @@ export default function Contact() {
     }
 
     return (
-        <div>
-            <h1>Contact Page</h1>
-            <form onSubmit={handleSubmit}>
-                {formError && <div className="alert alert-danger">{formError}</div>}
-                {formSuccess && <div className="alert alert-success">Email has been sent!</div>}
-                <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="name" value={name} onChange={(event) => setName(event.target.value)} />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={email} onChange={(event) => setEmail(event.target.value)} />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="message" className="form-label">Message</label>
-                    <textarea className="form-control" id="message" value={message} onChange={(event) => setMessage(event.target.value)} />
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+        <div className="center">
+            <div className='contact-form'>
+                <h1>Contact Page</h1>
+                <form onSubmit={handleSubmit}>
+
+                    {/* The warnings and success notifications */}
+
+                    {formError && <div className="alert alert-danger">{formError}</div>}
+                    {formSuccess && <div className="alert alert-success">Email has been sent!</div>}
+
+                    {/* The name input */}
+
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Name</label>
+                        <input type="text" className="form-control" id="name" placeholder="Enter Your Name" value={name} onChange={(event) => setName(event.target.value)} />
+                    </div>
+
+                    {/* The Email Input */}
+
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="email" placeholder="email@gamil.com" aria-describedby="emailHelp" value={email} onChange={(event) => setEmail(event.target.value)} />
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+
+                    {/* The Message Input */}
+
+                    <div className="mb-3">
+                        <label htmlFor="message" className="form-label">Message</label>
+                        <textarea className="form-control" id="message" placeholder="Write Message Here" value={message} onChange={(event) => setMessage(event.target.value)} />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </div>
     );
 }
