27be002322a75022bc9d1089e1313bef2d3c3205
Connor812 <connorsavoy812@gmail.com>
2023-03-23 23:01:57 -0400

    formated code

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -12,7 +12,7 @@ export default function Home() {
                 <img className='profile-img' src={profileImg} alt='profile' />
                 <div className='description'>
                     <div>
-                        Hello everyone! My name is Connor Savoy and I am from Langton Ontario! I'm currently enrolled at the University of Toronto in a full-stack coding bootcamp. I enrolled in this course because of my love for technology and computers. Coding is an amazing way to expand on those horizons and build something amazing from at first glace, looks likea bunch of mumbo-gumbo.
+                        Hello everyone! My name is Connor Savoy and I am from Langton Ontario! I'm currently enrolled at the University of Toronto in a full-stack coding bootcamp. I enrolled in this course because of my love for technology and computers. Coding is an amazing way to expand on those horizons and build something amazing from at first place, looks like a bunch of mumbo-jumbo.
                     </div>
                     <div>
                         I love the problem solving aspect of coding. It always keeps me on my toes and looking for new inovative ways to write code and solve my problems.
